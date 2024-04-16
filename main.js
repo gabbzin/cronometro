720a7942d007ea44cbfcfe8d675153830543d519
Gabriel P. <cgabrielpimentacruz111@gmail.com>
2024-04-16 20:43:13 -0300

    Primeiros passos

---
+++ main.js
@@ -0,0 +1,17 @@
+let segundos = document.getElementById('segundos')
+let minutos = document.getElementById('minutos')
+let horas = document.getElementById('horas')
+
+let botoes = document.querySelectorAll('button')
+
+buttons.forEach(element => {
+    element.addEventListener('click', (b) => {
+        if(b.target.innerText == 'iniciar'){
+            let tempoSegundo = 0;
+            let tempoMinuto = 0;
+            let tempoHora = 0;
+
+            while(
+        }
+    })
+})
